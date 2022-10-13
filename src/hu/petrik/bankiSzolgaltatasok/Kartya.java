13409bbeb9cfae7020a4513d92bfe2e36d7b1ff9
Zsombor Makker <makker.zsombor@gmail.com>
2022-10-13 09:29:42 +0200

    MegtakaritasiSzamla java done + Kartya java created

---
+++ src/hu/petrik/bankiSzolgaltatasok/Kartya.java
@@ -0,0 +1,20 @@
+package hu.petrik.bankiSzolgaltatasok;
+
+public class Kartya extends BankiSzolgaltatas{
+    private Szamla szamla;
+    private String kartyaSzam;
+
+    public Kartya(Tulajdonos tulajdonos, Szamla szamla, String kartyaSzam) {
+        super(tulajdonos);
+        this.szamla = szamla;
+        this.kartyaSzam = kartyaSzam;
+    }
+
+    public String getKartyaSzam() {
+        return kartyaSzam;
+    }
+
+    public boolean vasarlas(int osszeg){
+        return ;
+    }
+}
---
+++ src/hu/petrik/bankiSzolgaltatasok/MegtakaritasiSzamla.java
@@ -0,0 +1,33 @@
+package hu.petrik.bankiSzolgaltatasok;
+
+public class MegtakaritasiSzamla extends Szamla{
+    private double kamat;
+    public static double alapkamat = 1.1;
+
+    public MegtakaritasiSzamla(Tulajdonos tulajdonos) {
+        super(tulajdonos);
+        kamat = alapkamat;
+    }
+
+    @Override
+    public boolean kivesz(int osszeg) {
+        if (getAktualisEgyenleg() - osszeg >= 0){
+            this.aktualisEgyenleg -= osszeg;
+            return true;
+        }
+        return false;
+    }
+
+    public double getKamat() {
+        return kamat;
+    }
+
+    public void setKamat(double kamat) {
+        this.kamat = kamat;
+    }
+
+    public void kamatJovairas(){
+        //idk ide mit kell csinálni
+        this.aktualisEgyenleg = (int) (getAktualisEgyenleg() * this.kamat);
+    }
+}
