ab56d2e7f366322eebb70271b682e67309fb6c80
Zsombor Makker <makker.zsombor@gmail.com>
2022-10-13 08:26:17 +0200

    Tulajdonos java done

---
+++ src/hu/petrik/bankiSzolgaltatasok/BankiSzolgaltatas.java
@@ -0,0 +1,5 @@
+package hu.petrik.bankiSzolgaltatasok;
+
+public class BankiSzolgaltatas {
+
+}
---
+++ src/hu/petrik/bankiSzolgaltatasok/Tulajdonos.java
@@ -0,0 +1,17 @@
+package hu.petrik.bankiSzolgaltatasok;
+
+public final class Tulajdonos {
+    private String tulajdonos;
+
+    public Tulajdonos(String tulajdonos) {
+        this.tulajdonos = tulajdonos;
+    }
+
+    public String getTulajdonos() {
+        return tulajdonos;
+    }
+
+    public void setTulajdonos(String tulajdonos) {
+        this.tulajdonos = tulajdonos;
+    }
+}
